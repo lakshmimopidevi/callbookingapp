17021af8f3942065aa46373ddb508f3529851a2b
lakshmimopidevi <125748752+lakshmimopidevi@users.noreply.github.com>
2023-08-08 13:03:53 +0530

    Update uploadFile.js

--- Call Booking user-api/utils/uploadFile.js
+++ Call Booking user-api/utils/uploadFile.js
@@ -2,7 +2,7 @@ const multer  = require('multer');
 const {GridFsStorage} = require('multer-gridfs-storage');
 
 const storage = new GridFsStorage({ 
-    url: process.env.MONGO_URL,
+    url: process.env.mongodb+srv://lakshmivvsmarolix:84e966Q7utS2FGj3@cluster0.jhv9u6n.mongodb.net/?retryWrites=true&w=majority,
     file: (req, file) => {
         if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
             let filename = 'file_' + Date.now()
