723cec774c49ec5a8c8bfa14dd5c268daf9eed40
lakshmimopidevi <125748752+lakshmimopidevi@users.noreply.github.com>
2023-08-08 13:50:56 +0530

    Update uploadFile.js

--- Call Booking user-api/utils/uploadFile.js
+++ Call Booking user-api/utils/uploadFile.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
 
 const storage = new GridFsStorage({
-  url: process.env.MONGODB_URI, // Use the correct environment variable here
+  url: "mongodb+srv://lakshmivvsmarolix:84e966Q7utS2FGj3@cluster0.jhv9u6n.mongodb.net/?retryWrites=true&w=majority", // Use the correct environment variable here
   options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (req, file) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
