2d89acc0818001f8a3244fe6e5cbe1ac927840e1
lakshmimopidevi <125748752+lakshmimopidevi@users.noreply.github.com>
2023-08-08 13:51:39 +0530

    Update db.js

--- Call Booking user-api/config/db.js
+++ Call Booking user-api/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        mongoose.connect(mongodb+srv://lakshmivvsmarolix:84e966Q7utS2FGj3@cluster0.jhv9u6n.mongodb.net/?retryWrites=true&w=majority, err => {
+        mongoose.connect("mongodb+srv://lakshmivvsmarolix:84e966Q7utS2FGj3@cluster0.jhv9u6n.mongodb.net/?retryWrites=true&w=majority", err => {
             if (err) throw err;
             console.log('connected to MongoDB')
         });
